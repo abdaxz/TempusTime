b38a8879f120cc812849a5e57f0b499a2cdfb470
Tobias Andersson Gidlund <tobias.andersson.gidlund@gmail.com>
2016-05-03 11:17:51 +0200

    First complete mockup of main user interface.

--- TempusTime/src/tempustime/CourseManager.java
+++ TempusTime/src/tempustime/CourseManager.java
@@ -11,12 +11,42 @@ import javax.inject.Named;
 @Named
 @SessionScoped
 public class CourseManager implements Serializable {
+	private static final long serialVersionUID = 240902154191546462L;
 
 	private List<String> courseList = new ArrayList<String>(Arrays.asList(
 			"1IK213",
 			"1IK013",
 			"1DV508"
 			));
+	
+	private List<String> durationList = new ArrayList<String>(Arrays.asList(
+			"5 min",
+			"10 min",
+			"15 min",
+			"20 min",
+			"30 min",
+			"45 min",
+			"60 min",
+			"90 min",
+			"120 min"
+			));
+	
+	private List<String> activityList = new ArrayList<String>(Arrays.asList(
+			"Kursansvar",
+			"Planering",
+			"Handledning",
+			"Föreläsning",
+			"Planera föreläsning",
+			"Hålla labb",
+			"Labbkonstruktion",
+			"Labbrättning",
+			"Tentakonstruktion",
+			"Tentarättning"
+			));
+	
+	private String activity;
+	
+	private String duration;
 	
 	private String selectedCourse;
 	
@@ -30,6 +60,34 @@ public class CourseManager implements Serializable {
 
 	public void setSelectedCourse(String selectedCourse) {
 		this.selectedCourse = selectedCourse;
+	}
+
+	public String getDuration() {
+		return duration;
+	}
+
+	public void setDuration(String duration) {
+		this.duration = duration;
+	}
+	
+	public List<String> getDurationList() {
+		return durationList;
+	}
+
+	public String getActivity() {
+		return activity;
+	}
+
+	public void setActivity(String activity) {
+		this.activity = activity;
+	}
+	
+	public List<String> getActivityList() {
+		return activityList;
+	}
+	
+	public String result() {
+		return selectedCourse + " " + duration + " " + activity;
 	}
 		
 }
