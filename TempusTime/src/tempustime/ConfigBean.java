e3557e687e4d6871a969909a12b44e2c102d24cb
Tobias Andersson Gidlund <tobias.andersson.gidlund@gmail.com>
2016-05-23 15:45:04 +0200

    Selecting and editing now works (no db yet).

--- TempusTime/src/tempustime/ConfigBean.java
+++ TempusTime/src/tempustime/ConfigBean.java
@@ -10,6 +10,7 @@ import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import org.primefaces.event.RowEditEvent;
+import org.primefaces.event.SelectEvent;
 
 @Named
 @ViewScoped
@@ -18,6 +19,7 @@ public class ConfigBean implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private List<Course> courses = null;
 	private Course courseSelection;
+	private Course oldCourse = new Course();
 	
 	@EJB
 	private CourseBean theCourses;
@@ -39,8 +41,27 @@ public class ConfigBean implements Serializable {
 		this.courseSelection = courseSelection;
 	}
 	
+	public void onRowSelect(SelectEvent event) {
+		if(courseSelection != null) {
+			CoursePK tmp = new CoursePK();
+			
+			String tName = courseSelection.getId().getCourseName();
+			String tTerm = courseSelection.getId().getTerm();
+			int tYear = courseSelection.getId().getYear();
+			
+			tmp.setCourseName(tName);
+			tmp.setTerm(tTerm);
+			tmp.setYear(tYear);
+			
+			int tActive = courseSelection.getActive();
+			
+			oldCourse.setId(tmp);
+			oldCourse.setActive(tActive);
+		}
+	}
+	
 	public void onRowEdit(RowEditEvent event) {
-		FacesMessage msg = new FacesMessage("Course Edited", ((Course) event.getObject()).getId().getCourseName() + " was " + courseSelection.getId().getCourseName());
+		FacesMessage msg = new FacesMessage("Course Edited", ((Course) event.getObject()).getId().getCourseName() + " was " + oldCourse.getId().getCourseName());
         FacesContext.getCurrentInstance().addMessage(null, msg);
 	}
 }
