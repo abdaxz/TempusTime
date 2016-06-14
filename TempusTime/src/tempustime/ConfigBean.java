2a2c979248ba162004d84ea4b87fac271b7bdb18
Tobias Andersson Gidlund <tobias.andersson.gidlund@gmail.com>
2016-06-14 15:41:47 +0200

    Courses can be added.

--- TempusTime/src/tempustime/ConfigBean.java
+++ TempusTime/src/tempustime/ConfigBean.java
@@ -1,6 +1,7 @@
 package tempustime;
 
 import java.io.Serializable;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -24,11 +25,16 @@ public class ConfigBean implements Serializable {
 	private List<Course> courses = null;
 	private Course courseSelection;
 	private Course oldCourse = new Course();
-	
+	private int newCourseYear;
+	private String newCourseName;
+	private String newCourseTerm;
+	private int newCourseActive;
+	private boolean newCourseActiveBool;
+
 	@EJB
 	private CourseBean theCourses;
 	
-	private FacesContext context = FacesContext.getCurrentInstance();
+	//private FacesContext context = FacesContext.getCurrentInstance();
 	
 	@PostConstruct
 	public void init() {
@@ -96,5 +102,85 @@ public class ConfigBean implements Serializable {
 		FacesMessage msg = new FacesMessage("Kursen har ändrats", ((Course) event.getObject()).getId().getCourseName() + " är nu " + active);
         FacesContext.getCurrentInstance().addMessage(null, msg);
         theCourses.updateCourseStatus(courseSelection.getId(), ((Course) event.getObject()).getActive());
+	}
+	
+	public String getNewCourseName() {
+		return newCourseName;
+	}
+
+	public void setNewCourseName(String newCourseName) {
+		this.newCourseName = newCourseName;
+	}
+
+	public String getNewCourseTerm() {
+		return newCourseTerm;
+	}
+
+	public void setNewCourseTerm(String newCourseTerm) {
+		this.newCourseTerm = newCourseTerm;
+	}
+
+	public int getNewCourseActive() {
+		newCourseActive=0;
+		return newCourseActive;
+	}
+
+	public void setNewCourseActive(int newCourseActive) {
+		this.newCourseActive = newCourseActive;
+	}
+
+	public int getNewCourseYear() {
+		newCourseYear = Calendar.getInstance().get(Calendar.YEAR);
+		return newCourseYear;
+	}
+
+	public void setNewCourseYear(int newCourseYear) {
+		this.newCourseYear = newCourseYear;
+	}
+	
+	public void addNewCourse() {
+		
+		Course tmp = new Course();
+		CoursePK tmpPK = new CoursePK();
+		tmpPK.setCourseName(newCourseName);
+		tmpPK.setTerm(newCourseTerm);
+		tmpPK.setYear(newCourseYear);
+		tmp.setId(tmpPK);
+		tmp.setActive(newCourseActive);
+		
+		try {
+			theCourses.addNewCourse(tmp);
+			FacesMessage message = new FacesMessage("Kurs har lagts till", "Id: " + newCourseName);
+	        FacesContext.getCurrentInstance().addMessage(null, message);
+		} catch (Exception ex) {
+			FacesMessage msg = new FacesMessage("Ett fel uppstod", "Kursen kunde inte läggas till");
+			FacesContext.getCurrentInstance().addMessage(null, msg);
+		}
+	}
+	
+	/* public void onCourseAdded(SelectEvent event) {
+		Course theCourse = (Course) event.getObject();
+		
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Kurs har lagts till", "Id: " + theCourse.getId().getCourseName());
+        
+        FacesContext.getCurrentInstance().addMessage(null, message);
+	}*/
+
+	public boolean isNewCourseActiveBool() {
+		if(newCourseActive == 1)
+			newCourseActiveBool = true;
+		else
+			newCourseActiveBool = false;
+				
+		return newCourseActiveBool;
+	}
+
+	public void setNewCourseActiveBool(boolean newCourseActiveBool) {
+		if(newCourseActiveBool == true)
+			newCourseActive = 1;
+		else
+			newCourseActive = 0;
+		
+		this.newCourseActiveBool = newCourseActiveBool;
 	}
 }
--- TempusTime/src/tempustime/CourseBean.java
+++ TempusTime/src/tempustime/CourseBean.java
@@ -56,5 +56,9 @@ public class CourseBean {
     		em.remove(tmp);
     	}    	
     }
+    
+    public void addNewCourse(Course c) {
+    	em.persist(c);
+    }
 
 }
